fc873fcb1bc8934a5ef42a06ef904daedb488909
Alex Fritz <alexfritzcoach@gmail.comm>
2021-09-16 19:21:23 -0700

    Add booleans to the assignment

--- model-1.js
+++ model-1.js
@@ -11,3 +11,10 @@ var numberOfClimbs = 24;
 var easiestClimb = 5.7;
 var hardestClimb = 5.14;
 var myFavClimbGrade = 5.13;
+
+//Booleans
+var isItTall = true;
+var isItScary = true;
+var isTheHikeLong = true;
+var isThisBouldering = false;
+var canITopRope = false;
