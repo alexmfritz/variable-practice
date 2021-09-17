f47590615b58c7ca55f2d8b16916bf809542df6e
Alex Fritz <alexfritzcoach@gmail.comm>
2021-09-16 19:18:32 -0700

    Add numbers to to assignment

--- model-1.js
+++ model-1.js
@@ -5,3 +5,9 @@ var areaName = "Little Si";
 var cragName = "World Wall 2";
 var mostFamousClimb = "Aboriginie";
 var myFavClimb = "Chronic";
+
+//Numbers
+var numberOfClimbs = 24;
+var easiestClimb = 5.7;
+var hardestClimb = 5.14;
+var myFavClimbGrade = 5.13;
